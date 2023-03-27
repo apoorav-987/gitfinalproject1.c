3d5c13abf2da03fd09fc7c4934788013ee4476fe
arman-mahajan <123321292+arman-mahajan@users.noreply.github.com>
2023-03-27 11:16:45 +0530

    Add files via upload

--- scm project.c
+++ scm project.c
@@ -1,29 +1,4 @@
-// C program for the above approach
-#include <math.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-
-// Function to implement the game
-int game(char you, char computer)
-{
-	// If both the user and computer
-	// has choose the same thing
-	if (you == computer)
-		return -1;
-		// If user's choice is stone and
-	// computer's choice is paper
-	if (you == 's' && computer == 'p')
-		return 0;
-
-			// If user's choice is paper and
-			// computer's choice is stone
-			else if (you == 'p' && computer == 's') return 1;
-
-	// If user's choice is stone and
-	// computer's choice is scissor
-<<<<<<< HEAD
-	int main()
+int main()
 {
 	// Stores the random number
 	int n;
@@ -33,25 +8,12 @@ int game(char you, char computer)
 	// Chooses the random number
 	// every time
 	srand(time(NULL));
-=======
-	if (you == 's' && computer == 'z')
-		return 1;
 
-	// If user's choice is scissor and
-	// computer's choice is stone
-	else if (you == 'z' && computer == 's')
-		return 0;
+	// Make the random number less
+	// than 100, divided it by 100
+	n = rand() % 100;
 
-	// If user's choice is paper and
-	// computer's choice is scissor
-	if (you == 'p' && computer == 'z')
-		return 0;
-
-	// If user's choice is scissor and
-	// computer's choice is paper
-	else if (you == 'z' && computer == 'p')
-		return 1;
-}
-
-// Driver Code
->>>>>>> ce2a3fe0aedc28f2fdc192f2716aacf4769e2d10
+	// Using simple probability 100 is
+	// roughly divided among stone,
+	// paper, and scissor
+	if (n < 33)
